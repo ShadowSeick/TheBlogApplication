3fc955ea8b10c7f229c98aabc3eea17987ec4a8b
Adrián Muñoz González <alcolka@gmail.com>
2021-09-02 09:19:47 +0200

    Cleaning up zombie code

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -67,7 +67,6 @@ namespace TheBlogApplication.Controllers
             var pageNumber = page ?? 1;
             var pageSize = 5;
 
-            //var posts = _context.Posts.Where(p => p.BlogId == id).ToList();
             var posts = await _context.Posts
                 .Where(p => p.BlogId == id && p.ReadyStatus == ReadyStatus.ProductionReady)
                 .OrderByDescending(p => p.Created)
